625ac5733d32ccf0d52eca9cb95ede8bb2a41f72
d_i_m_y_l_k_a <130781436+dimasikks@users.noreply.github.com>
2024-05-14 18:58:21 +0300

    Update bot_for_download.py

--- bot_for_download.py
+++ bot_for_download.py
@@ -28,11 +28,11 @@ portDB=os.getenv('DB_PORT')
 databaseDB=os.getenv('DB_DATABASE')
 
 connection = psycopg2.connect(
-        user=userDB,
-        password=passwordDB,
-        host=hostDB,
-        port=portDB,
-        database=databaseDB
+        user="postgres",
+        password="Qq12345",
+        host="192.168.10.156",
+        port="5432",
+        database="base_1"
 )
 cursor = connection.cursor()
 
